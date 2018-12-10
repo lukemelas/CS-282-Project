c6a41dee25875896978b6a921485d5f134062930
Ubuntu <nlp@nlp-cloud-3.oamjvhiicztehe22zxxkimvwgf.xx.internal.cloudapp.net>
2018-12-10 18:45:57 +0000

    More improvements

--- gcn/dataloader.py
+++ gcn/dataloader.py
@@ -46,13 +46,84 @@ def load_cora(path='../data/cora/'):
     adj = scipy_to_torch_sparse(adj)
 
     # Save for GAE
-    pdb.set_trace()
     torch.save({'adj_orig': adj_orig.todense(), 'adj_i': adj.indices, 'adj_v': adj.values,
                 'adj_s': adj.shape, 'feats': feats, 'labels': labels, 
                 'idx_train': idx_train, 'idx_val': idx_val, 'idx_test': idx_test}, 
                 '../data/cora/preprocessed_gcn_data_for_gae.pth')
 
     return adj, feats, labels, idx_train, idx_val, idx_test
+
+def load_adv_data(node, path="../data/cora/", dataset="cora"):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
+                                        dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+    
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
+                                    dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    
+    labels = np.where(encode_onehot(idx_features_labels[:, -1]))[1]
+    new_adj = deepcopy(np.array(adj.todense()))
+    neighbours = (new_adj[node, :] > 0).astype(int)
+    neighbours[node] = 0
+    not_class = (labels != labels[node]).astype(int)
+    eligible = np.where(not_class - neighbours > 0)[0] #indices to choose new edges from
+    ineligible = np.where(neighbours)[0] #indices to remove edges from
+    
+    for i in ineligible:
+        if i in eligible:
+            import pdb; pdb.set_trace()
+        assert(i not in eligible)
+
+    num_perturb = min(int(sum(neighbours) / 2 + 1), len(eligible), len(ineligible))
+    to_add = eligible[np.random.choice(len(eligible), num_perturb, replace=False)]
+    to_remove = ineligible[np.random.choice(len(ineligible), num_perturb, replace=False)]
+    print("Added: ", to_add, "Removed: ", to_remove)
+
+    for idx in to_add:
+        assert(new_adj[node,idx] == 0)
+        assert(new_adj[idx,node] == 0)
+        new_adj[node,idx] = 1
+        new_adj[idx,node] = 1
+
+    for idx in to_remove:
+        assert(new_adj[node,idx] == 1)
+        assert(new_adj[idx,node] == 1)
+        new_adj[node,idx] = 0
+        new_adj[idx,node] = 0
+    
+    adj = sp.csr_matrix(new_adj)
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(labels)
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
 
 # For testing
 if __name__ == '__main__':
--- variational hypergat/layers.py
+++ variational hypergat/layers.py
@@ -65,8 +65,10 @@ class GraphAttentionLayer(nn.Module):
         self.out_features = out_features
         self.alpha = alpha
         self.concat = concat
-
+        
+        print((in_features, out_features))
         self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
+        
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
         
         # self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
